3d4a4974ee8846fe2662fb79a6ad5d699b9db4a1
artem-nazarov-idf <artem.nazarov@idfeurasia.com>
2022-06-22 14:22:17 +0700

    fix

--- src/main/kotlin/com/framework/test/config/provider/ApplicationConfigProvider.kt
+++ src/main/kotlin/com/framework/test/config/provider/ApplicationConfigProvider.kt
@@ -2,12 +2,12 @@ package com.framework.test.config.provider
 
 import com.framework.test.constants.FileType
 
-class ApplicationConfigProvider(private val fileName: String, private val fileType: FileType) {
-
+class ApplicationConfigProvider(fileName: String, private val fileType: FileType) {
+  private val file = FileProvider(fileName).getFileFromPath()
   fun makeApplicationConfig(): ApplicationConfigInterface {
     return when (fileType) {
-      FileType.JSON -> ApplicationConfigJson(FileProvider(fileName).getFileFromPath())
-      FileType.YAML -> ApplicationConfigYaml(FileProvider(fileName).getFileFromPath())
+      FileType.JSON -> ApplicationConfigJson(file)
+      FileType.YAML -> ApplicationConfigYaml(file)
 
     }
   }
