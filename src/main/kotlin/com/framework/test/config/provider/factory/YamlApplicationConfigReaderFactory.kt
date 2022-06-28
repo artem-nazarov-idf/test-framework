98e353914e52fd6f86375e25c6f6eae1b3769ad2
artem-nazarov-idf <artem.nazarov@idfeurasia.com>
2022-06-28 17:17:11 +0700

    specified the type of the variables

--- src/main/kotlin/com/framework/test/config/provider/factory/YamlApplicationConfigReaderFactory.kt
+++ src/main/kotlin/com/framework/test/config/provider/factory/YamlApplicationConfigReaderFactory.kt
@@ -6,12 +6,13 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import com.framework.test.config.model.ApplicationConfig
 import com.framework.test.constants.FileNames
 import com.framework.test.file.reader.FileProvider
+import java.io.File
 
 class YamlApplicationConfigReaderFactory(
   private val fileProvider: FileProvider = FileProvider()
 ) : ApplicationConfigReader {
   override val configFilePath: String = "authData/${FileNames.BASIC_AUTH_FILE_NAME.value}.yaml"
-  private val file by lazy { fileProvider.getFileFromPath(configFilePath) }
+  private val file: File by lazy { fileProvider.getFileFromPath(configFilePath) }
 
   override fun getConfigFromFile(): ApplicationConfig {
     val mapper = ObjectMapper(YAMLFactory()).findAndRegisterModules() // worksWithJsonToo
