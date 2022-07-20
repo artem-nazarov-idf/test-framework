26842d7e27be2980bac4d3110be269d25581e165
artem-nazarov-idf <artem.nazarov@idfeurasia.com>
2022-07-20 12:42:50 +0700

    removed unnecessary data from the static context

--- src/main/kotlin/com/framework/test/context/ContextAccess.kt
+++ src/main/kotlin/com/framework/test/context/ContextAccess.kt
@@ -1,5 +1,6 @@
 package com.framework.test.context
 
+import com.framework.test.constants.CrmUserRole
 import com.framework.test.context.constant.MyStaticContext
 import com.framework.test.context.constant.StaticContextHolder
 import com.framework.test.context.dynamic.DynamicContextHolder
@@ -11,8 +12,8 @@ import com.framework.test.model.config.StubConfig
 
 internal fun staticContext(): MyStaticContext = StaticContextHolder.getContext()
 internal fun applicationConfig(): ApplicationConfig = staticContext().applicationConfig
-internal fun stubConfig(): StubConfig = staticContext().stubConfig
-internal fun administrator(): CrmUser = staticContext().administrator
+internal fun stubConfig(): StubConfig = applicationConfig().stub
+internal fun administrator(): CrmUser = applicationConfig().crmUsers!![CrmUserRole.ADMINISTRATOR]!!
 internal fun sessionContext(): SessionContext = staticContext().sessionContext
 internal fun dynamicContext(): MyDynamicContext = DynamicContextHolder.getContext()
 internal fun jSessionId(): String? = dynamicContext().getJSessionIdValue()
